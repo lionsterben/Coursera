9a2cb8a8a370aa9807112b4ba802f7ffbe16a3d4
dawei <f1412465878@gmail.com>
2018-04-16 23:03:15 +0800

    delete useless file in UWmachinelearning

---
+++ princeton_algorithm/test_order/src/first.java
@@ -0,0 +1,19 @@
+/**
+ * Created by david on 2018/4/4.
+ */
+public class first {
+    private int a = 3;
+    private int b = 0;
+    public void haha(){
+        System.out.println("what"+ a);
+    }
+    public void get_b(){
+         haha();
+    }
+    public static void main(String[] args){
+        first a = new first();
+        a.haha();
+    }
+}
+
+
